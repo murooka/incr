f4fb31e11a78bef7e9859e0ca796b820bf0fd1dc
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2016-04-19 01:52:01 +0900

    Fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,16 @@ var incr = require('./incr');
 
 $(function () {
     (function () {
-        var count = 0;
         var $button = $('#incr-button');
         var $field = $('#incr-field');
-        $button.click(function () {
-                count = incr(parseInt($field.text()));
+        var count = parseInt($field.data('default'));
+        var render = function () {
             $field.text(count);
+        };
+        $button.click(function () {
+            count = incr(count);
+            render();
         });
+        render();
     })();
 });
